93fbfc65cd453f40f7bf014cba482f11c7e56774
justin <justinm48000@gmail.com>
2024-01-30 14:05:21 +0300

    update changes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom'
-import Home from './components/Home'
-import About from './components/About'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <div className='App'>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-            </ul>
-          </nav>
+        <div className="App">
+          <Navbar />
           <Routes>
             <Route path="/" exact component={Home} />
             <Route path="/about" exact component={About} />
+            <Route path="/navbar" exact component={Navbar} />
           </Routes>
         </div>
       </Router>
