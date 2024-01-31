87a7506ed00e62c6bf9e31f1732e95bf6f0f4188
justin <justinm48000@gmail.com>
2024-01-31 11:27:17 +0300

    chat section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,17 @@
+// Home.js
 import React from 'react';
+import './Home.css';
+import Message from './Message';
 
 const Home = () => {
   return (
-    <div>
+    <div className='home-display'>
       <h2>Welcome to the Chat App</h2>
-      <p>This is the home page.</p>
+      <div className="chat-container">
+        <Message content="Sent message goes here" type="sent" timestamp="12:34 PM" />
+        <Message content="Received message goes here" type="received" timestamp="12:35 PM" />
+        {/* Add more messages as needed */}
+      </div>
     </div>
   );
 };
